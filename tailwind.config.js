42ab35f37eaaf476963de2ecbfc01384b2d5e27a
bng16 <biswajitnag161619@gmail.com>
2024-08-19 13:34:42 +0530

    minor change

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: ["New Amsterdam", "system-ui"],
+    },
   },
   plugins: [],
 }
