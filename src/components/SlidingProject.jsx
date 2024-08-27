11488edf3d0d95d4271e6297fb4e84e91f845737
bng16 <biswajitnag161619@gmail.com>
2024-08-27 23:55:00 +0530

    sliding page before projects done

--- src/components/SlidingProject.jsx
+++ src/components/SlidingProject.jsx
@@ -1,9 +1,27 @@
 function SlidingProject() {
   return (
-    <div className="w-full h-screen bg-primary whitespace-nowrap flex items-center overflow-hidden  relative">
-      <h1 className="font-black text-[25vw] leading-none absolute left-[110%]">FROM CONCEPT TO CREATION, EXPLORE MY WORK...</h1>
+    <div className="relative w-full h-screen bg-primary whitespace-nowrap flex items-center overflow-hidden">
+      <div className="absolute left-[100%]">
+        <h1 className="font-black text-[30vw] leading-none z-[0] ">FROM CONCEPT TO CREATION, EXPLORE MY WORK<span className="text-tertiary">...</span></h1>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[12%] left-[7.5%]"></div>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[75%] left-[17.5%]"></div>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[12%] left-[25%]"></div>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[75%] left-[32.5%]"></div>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[12%] left-[44.4%]"></div>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[75%] left-[53.5%]"></div>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[12%] left-[63%]"></div>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[75%] left-[75.5%]"></div>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[12%] left-[87.5%]"></div>
+        <div className="absolute w-[10vw] h-[6vw] bg-yellow-200 rounded-lg hover:rounded-none ease-in-out duration-300 top-[75%] left-[95.5%]"></div>
+
+      </div>
+        
+
     </div>
   )
 }
 
 export default SlidingProject
+
+
+// to : -625%
