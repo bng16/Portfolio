cf298230de5fb86413e7bb883b31d43d55e68e59
bng16 <biswajitnag161619@gmail.com>
2024-08-30 22:08:11 +0530

    hero button initial animation done [have to adjust click animation] {all ok}

--- src/pages/hero/HeroButton.jsx
+++ src/pages/hero/HeroButton.jsx
@@ -1,18 +1,72 @@
-import gsap from 'gsap';
-import { useRef, useEffect } from 'react';
+// import { useRef, useEffect } from "react";
+// import gsap from "gsap";
 
+// function HeroButton() {
+//   const homeButtonRef = useRef(null);
+
+//   useEffect(() => {
+//     if (homeButtonRef.current) {
+//       gsap.fromTo(
+//         homeButtonRef.current,
+//         { y: 100, opacity: 0 },
+//         {
+//           y: 0,
+//           opacity: 1,
+//           delay: 0.1,
+//           duration: 3,
+//         }
+//       );
+//     }
+//   }, []);
+
+//   return (
+//     <div className="overflow-hidden">
+//       <button
+//         ref={homeButtonRef}
+//         className='relative cursor-pointer inline-block px-[2vw] py-[0.6vw] rounded-md font-bold uppercase border-[1px] bg-tertiary text-primary border-tertiary transition-all duration-500 ease-in-out hover:text-tertiary hover:bg-primary active:scale-90 z-10'
+//         >
+//         CONTACT ME
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default HeroButton;
+
+
+
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
 
 function HeroButton() {
-  return (
-    <button
-      className='relative cursor-pointer inline-block px-[2vw] py-[0.6vw] rounded-md font-bold uppercase border-[1px] bg-tertiary text-primary border-tertiary transition-all duration-500 ease-in-out hover:text-tertiary hover:bg-primary active:scale-90 z-10'
-    >
-      CONTACT ME
+  const homeButtonRef = useRef(null);
 
-    </button>
-  )
+  useEffect(() => {
+    if (homeButtonRef.current) {
+      gsap.fromTo(
+        homeButtonRef.current,
+        { y: 100, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          delay: 0.1,
+          duration: 3,
+          ease: "power3.out",
+        }
+      );
+    }
+  }, []);
+
+  return (
+    <div className="overflow-hidden">
+      <button
+        ref={homeButtonRef}
+        className='relative cursor-pointer inline-block px-[2vw] py-[0.6vw] rounded-md font-bold uppercase border-[1px] bg-tertiary text-primary border-tertiary transition-all duration-500 ease-in-out hover:text-tertiary hover:bg-primary active:scale-90 z-10'
+        >
+        CONTACT ME
+</button>
+    </div>
+  );
 }
 
-export default HeroButton
-
-
+export default HeroButton;
