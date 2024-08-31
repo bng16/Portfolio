cff66abaaf183da3fdaebd934be6b029dacceb34
bng16 <biswajitnag161619@gmail.com>
2024-08-31 21:26:19 +0530

    completed the projects part {all ok}

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,9 +1,15 @@
-import { useRef } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useGSAP } from '@gsap/react';
+import { useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
+
+import img1 from '../../assets/Projects/1.jpg';
+import img2 from '../../assets/Projects/2.jpg';
+import img3 from '../../assets/Projects/3.jpg';
+import img4 from '../../assets/Projects/4.jpg';
+import img5 from '../../assets/Projects/5.jpg';
 
 function Projects() {
   const mainSectionRef = useRef();
@@ -18,95 +24,72 @@ function Projects() {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: mainSectionRef.current,
-        // markers: true,
-        start: '38% 50%',
-        end: '1200% 50%',
-        scrub: 1,
+        start: "38% 50%",
+        end: "1400% 50%",
+        scrub: 3,
         pin: true,
       },
     });
 
-    tl
-    // .from(textRef.current, {
-    //   scale: 0,
-    //   opacity: 0,
-    // }, 'start')
-
-    .to(textRef.current, {
-      top: '5%',
-      scale: 0.5,
-    }, 'a')
-    .to(image1Ref.current, {
-      top: '30%',
-    }, 'a')
-
-    .to(image2Ref.current, {
-      top: '34%',
-    }, 'b')
-    .to(image1Ref.current, {
-      width: '50%',
-      height: '50vh',
-    }, 'b')
-
-    .to(image3Ref.current, {
-      top: '38%',
-    }, 'c')
-    .to(image2Ref.current, {
-      width: '55%',
-      height: '55vh',
-    }, 'c')
-
-    .to(image4Ref.current, {
-      top: '42%',
-    }, 'd')
-    .to(image3Ref.current, {
-      width: '60%',
-      height: '60vh',
-    }, 'd')
-
-    .to(image5Ref.current, {
-      top: '46%',
-    }, 'e')
-    .to(image4Ref.current, {
-      width: '65%',
-      height: '65vh',
-    }, 'e')
+    tl.from(textRef.current, { scale: 0, opacity: 0 }, "start")
+      .to(textRef.current, { top: "5%", scale: 0.5 }, "a")
+      .to(image1Ref.current, { top: "30%" }, "a")
+      .to(image2Ref.current, { top: "34%" }, "b")
+      .to(image1Ref.current, { width: "50%", height: "50vh" }, "b")
+      .to(image3Ref.current, { top: "38%" }, "c")
+      .to(image2Ref.current, { width: "55%", height: "55vh" }, "c")
+      .to(image4Ref.current, { top: "42%" }, "d")
+      .to(image3Ref.current, { width: "60%", height: "60vh" }, "d")
+      .to(image5Ref.current, { top: "46%" }, "e")
+      .to(image4Ref.current, { width: "65%", height: "65vh" }, "e")
+      .to(image5Ref.current, { bottom:'0px',width: "100%", height: "100vh" }, "f");
   }, []);
 
   return (
-    <main ref={mainSectionRef} className='relative w-full h-[150vh] flex flex-col items-center bg-primary'>
-      <h1 ref={textRef} className='font-black absolute top-[20vh] text-secondary text-[10vw]'>
-        <span className='text-tertiary'>!</span> PROJECTS <span className='text-tertiary'>!</span>
+    <main ref={mainSectionRef} className="relative w-full h-[150vh] flex flex-col items-center bg-primary">
+      <h1 ref={textRef} className="font-black absolute top-[20vh] text-secondary text-[10vw]">
+        <span className="text-tertiary">!</span> PROJECTS <span className="text-tertiary">!</span>
       </h1>
-      <div
-        ref={image1Ref}
-        className='w-[80%] h-[80vh] bg-red-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl'
-        // style={{ backgroundImage: 'url(./images/1.jpg)' }}
-      ></div>
 
-      <div
-        ref={image2Ref}
-        className='w-[80%] h-[80vh] bg-blue-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl'
-        // style={{ backgroundImage: 'url(./images/2.jpg)' }}
-      ></div>
+      <a href="https://www.google.co.in/" target="_blank">
+        <div
+          ref={image1Ref}
+          className="w-[80%] h-[80vh] bg-red-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl"
+          style={{ backgroundImage: `url(${img1})` }}
+        ></div>
+      </a>
 
-      <div
-        ref={image3Ref}
-        className='w-[80%] h-[80vh] bg-green-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl'
-        // style={{ backgroundImage: 'url(./images/3.jpg)' }}
-      ></div>
+      <a href="https://www.google.co.in/" target="_blank">
+        <div
+          ref={image2Ref}
+          className="w-[80%] h-[80vh] bg-blue-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl"
+          style={{ backgroundImage: `url(${img2})` }}
+        ></div>
+      </a>
 
-      <div
-        ref={image4Ref}
-        className='w-[80%] h-[80vh] bg-yellow-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl'
-        // style={{ backgroundImage: 'url(./images/4.jpg)' }}
-      ></div>
+      <a href="https://www.google.co.in/" target="_blank">
+        <div
+          ref={image3Ref}
+          className="w-[80%] h-[80vh] bg-green-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl"
+          style={{ backgroundImage: `url(${img3})` }}
+        ></div>
+      </a>
 
-      <div
-        ref={image5Ref}
-        className='w-[80%] h-[80vh] bg-red-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl'
-        // style={{ backgroundImage: 'url(./images/5.jpg)' }}
-      ></div>
+      <a href="https://www.google.co.in/" target="_blank">
+        <div
+          ref={image4Ref}
+          className="w-[80%] h-[80vh] bg-yellow-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl"
+          style={{ backgroundImage: `url(${img4})` }}
+        ></div>
+      </a>
+
+      <a href="https://www.google.co.in/" target="_blank">
+        <div
+          ref={image5Ref}
+          className="w-[80%] h-[80vh] bg-red-200 bg-cover bg-center absolute top-[130%] left-[50%] -translate-x-1/2 -translate-y-1/2 rounded-xl"
+          style={{ backgroundImage: `url(${img5})` }}
+        ></div>
+      </a>
     </main>
   );
 }
