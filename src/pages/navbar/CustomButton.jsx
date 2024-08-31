6718f6fd5a8dc34f1e187ce82ea407324b2d47f8
bng16 <biswajitnag161619@gmail.com>
2024-08-31 21:29:07 +0530

    minor change in the resume button {all ok}

--- src/pages/navbar/CustomButton.jsx
+++ src/pages/navbar/CustomButton.jsx
@@ -11,7 +11,7 @@ function CustomButton() {
       download="BiswajitNag.pdf" // The name of the file when downloaded
     className='relative cursor-pointer inline-block px-[2vw] py-[0.6vw] rounded-md font-bold uppercase border-[1px] bg-tertiary text-primary border-tertiary transition-all duration-500 ease-in-out hover:text-tertiary hover:bg-primary active:scale-90 z-10'
   >
-    CONTACT ME
+    RRESUME
 
   </a>
   );
