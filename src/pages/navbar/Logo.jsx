dbfe6ab80b5ee1ec0a15494a7a516725aea05443
bng16 <biswajitnag161619@gmail.com>
2024-09-02 03:37:29 +0530

    spinnig logo sone {all ok}

--- src/pages/navbar/Logo.jsx
+++ src/pages/navbar/Logo.jsx
@@ -1,13 +1,36 @@
+import { useRef, useState, useEffect } from "react";
+import gsap from "gsap";
+
 function Logo() {
+  const logoRef = useRef(null);
+  const [logoState, setLogoState] = useState(false);
+
+  useEffect(() => {
+    gsap.to(logoRef.current, {
+      rotate: logoState ? 720 : -720,
+      duration: 1,
+      ease: "power2.inOut" // Added ease for smoother rotation
+    });
+  }, [logoState]);
+
   return (
-    <>
-    <div id="logo-section" className=" w-[7vw] flex justify-around items-center  cursor-pointer">
-        <img className=' w-[2vw] ' src="src\assets\logo\BngLogo.svg" alt="" />
-        <h1 className="font-black text-[2vw] tracking-tighter leading-none">Bng<span className="text-tertiary">.</span></h1>
-      </div>
-    </>
-  )
+    <div
+      id="logo-section"
+      className="w-[7vw] flex justify-around items-center cursor-pointer"
+      onMouseEnter={() => setLogoState(true)}
+      onMouseLeave={() => setLogoState(false)}
+    >
+      <img
+        ref={logoRef}
+        className="w-[2vw]"
+        src="src/assets/logo/BngLogo.svg" // Updated path format
+        alt="Logo"
+      />
+      <h1 className="font-black text-[2vw] tracking-tighter leading-none">
+        Bng<span className="text-tertiary">.</span>
+      </h1>
+    </div>
+  );
 }
 
-export default Logo
-
+export default Logo;
