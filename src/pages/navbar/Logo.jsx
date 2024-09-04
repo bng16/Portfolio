d1036f663fdf8e7dc1a92039e0d766b6a8288878
bng16 <biswajitnag161619@gmail.com>
2024-09-04 19:57:51 +0530

    icon and logo path updated {all ok}

--- src/pages/navbar/Logo.jsx
+++ src/pages/navbar/Logo.jsx
@@ -1,16 +1,17 @@
 import { useRef, useState, useEffect } from "react";
 import gsap from "gsap";
 
+import bngLogo from "src/assets/logo/BngLogo.svg";
+
 function Logo() {
   const logoRef = useRef(null);
   const [logoState, setLogoState] = useState(false);
-  
 
   useEffect(() => {
     gsap.to(logoRef.current, {
       rotate: logoState ? 720 : -720,
       duration: 1,
-      ease: "power2.inOut" // Added ease for smoother rotation
+      ease: "power2.inOut", // Added ease for smoother rotation
     });
   }, [logoState]);
 
@@ -21,12 +22,8 @@ function Logo() {
       onMouseEnter={() => setLogoState(true)}
       onMouseLeave={() => setLogoState(false)}
     >
-      <img
-        ref={logoRef}
-        className="w-[2vw]"
-        src="src/assets/logo/BngLogo.svg"
-        alt="Logo"
-      />
+      <img ref={logoRef} className="w-[2vw]" src={bngLogo} alt="Logo" />
+
       <h1 className="font-black text-[2vw] tracking-tighter leading-none">
         Bng<span className="text-tertiary">.</span>
       </h1>
@@ -35,4 +32,3 @@ function Logo() {
 }
 
 export default Logo;
-
